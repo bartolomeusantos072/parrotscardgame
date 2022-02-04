db4acedbf7d6e19a64b68b5e931544afa3b8d7de
Uenderss <meajudabarto@gmail.com>
2022-02-04 16:12:19 -0300

    colocar lista de imagens no javascript

---
+++ jscript/script.js
@@ -0,0 +1,10 @@
+const listaImagens = [
+    "imagem/bobrossparrot.gif",
+    "imagem/explodyparrot.gif",
+    "imagem/metalparrot.gif",
+    "imagem/revertitparrot.gif",
+    "imagem/tripletsparrot.gif",
+    "imagem/unicornparrot.gif",
+    "imagem/fiestaparrot.gif"
+  ];
+  
