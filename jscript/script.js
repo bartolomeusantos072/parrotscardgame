6ee522a72b77632fc69d3eb79a3aedd4d8b00e0a
Uenderss <meajudabarto@gmail.com>
2022-02-04 16:32:05 -0300

    implementaçao do CSS do body, h1, section e carta

--- jscript/script.js
+++ jscript/script.js
@@ -7,13 +7,26 @@ const listaImagens = [
     "imagem/unicornparrot.gif",
     "imagem/fiestaparrot.gif"
   ];
-  
+  let quantidade = 0;
+
   function iniciarJogo(){
-    let quantidade = 0;
+    
         do {
             quantidade = prompt("Escolha uma quantidade de cartas");
           } while (quantidade < 4 || quantidade > 14 || quantidade % 2 === 1);
           
     }
+darAsCartas(quantidade);  
+ 
+function darAsCartas(quantidade) {
     
+    for (let i = 0; i < quantidade; i++) {
+       document.querySelector(".jogo-da-memoria").innerHTML += `
+        <div class="carta" onclick="escolherCarta(this)">
+          <img class="img-frente" src="imagem/front.png"/>
+          <img class="img-verso" src=${imgLista[i].src}/>
+        </div>`;
+    }
+    
+  }   
     
